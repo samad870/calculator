e9211719bd32a034ab0376c1e2eae98bf0716f68
Samad Malik <samadmalikz123@gmail.com>
2024-11-08 08:55:59 -0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function handleAllClear() {
+  document.getElementById("result").value = "";
+}
+
+function dis(val) {
+  document.getElementById("result").value =
+    document.getElementById("result").value + val;
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+  let y = math.evaluate(x);
+  document.getElementById("result").value = y;
+}
